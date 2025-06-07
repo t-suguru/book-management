ffa9e23f543416df0e4271f770268060b28d71ae
t-suguru <72339566+t-suguru@users.noreply.github.com>
2025-06-07 17:09:08 +0900

    flywayの動作する環境とDBの作成 (#5)
    * flywayの動作する環境とDBの作成
    
    - flywayが動作するように設定
    - application.propertiesからymlへの変更
    - 初期DB構築用のマイグレーションファイルの作成
    
    * postgresのバージョン指定 テストコードのインポート修正

--- src/test/kotlin/io/github/t_suguru/book_management/HelloControllerTest.kt
+++ src/test/kotlin/io/github/t_suguru/book_management/HelloControllerTest.kt
@@ -2,8 +2,7 @@ package io.github.t_suguru.book_management
 
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
-import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
